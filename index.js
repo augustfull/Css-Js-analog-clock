41b599c05537b90891552c7d015cde4390b4841f
augustfull <javadroosta1998@gmail.com>
2022-01-19 22:24:18 +0330

    Bug Fix
    Reduced transition duration and added 1 degree to transform rotate function

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const seconds = now.getSeconds();
     const minutes = now.getMinutes();
     const hours = now.getHours();
-    const secondDegrees = (seconds / 60) * 360 + 90;
-    const minuteDegrees = (minutes / 60) * 360 + 90;
-    const hourDegrees = (hours / 12) * 360 + 90;
-    
+    const secondDegrees = (seconds / 60) * 360 + 91;
+    const minuteDegrees = (minutes / 60) * 360 + 91;
+    const hourDegrees = (hours / 12) * 360 + 91;
+  
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
     minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
