0773c5c81e4545dc98431961c2534c12e8d3a1c8
augustfull <javadroosta1998@gmail.com>
2022-01-19 21:07:50 +0330

    Adds HTML,CSS,JS code

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const secondHand = document.querySelector(".hand.second-hand");
+  const minuteHand = document.querySelector(".hand.minute-hand");
+  const hourHand = document.querySelector(".hand.hour-hand");
+
+  function getTime() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+    const secondDegrees = (seconds / 60) * 360 + 90;
+    const minuteDegrees = (minutes / 60) * 360 + 90;
+    const hourDegrees = (hours / 12) * 360 + 90;
+    
+    secondHand.style.transform = `rotate(${secondDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  }
+
+  setInterval(getTime, 1000);
 });
